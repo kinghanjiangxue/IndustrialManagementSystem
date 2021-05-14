6a425276b2f1dd611503a39ca7d5699e9710b646
元 <123@163.com>
2021-05-15 01:40:34 +0800

    添加导航功能

---
+++ lib/data/drawer_items.dart
@@ -0,0 +1,14 @@
+import 'package:universe/model/drawer_item.dart';
+import 'package:flutter/material.dart';
+
+final itemsFirst = [
+  DrawerItem(title: '柱状图', icon: 'images/histogram.png'),
+  DrawerItem(title: '饼状图', icon: 'images/pie.png'),
+  DrawerItem(title: '昨日产量', icon: 'images/total.png'),
+  DrawerItem(title: '历史记录', icon: 'images/history.png'),
+];
+
+final itemsSecond = [
+  DrawerItem(title: 'Deployment', icon: 'images/histogram.png'),
+  DrawerItem(title: 'Resources', icon: 'images/history.png'),
+];
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:universe/provider/navigation_provider.dart';
+import 'package:universe/widge/navigation_drawer_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Navigation Drawer';
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+    create: (context) => NavigationProvider(),
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: MainPage(),
+    ),
+  );
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    drawer: NavigationDrawerWidget(),
+    appBar: AppBar(
+      backgroundColor: Color(0xff3875F6),
+      title: Text(MyApp.title),
+      centerTitle: true,
+    ),
+  );
+}
