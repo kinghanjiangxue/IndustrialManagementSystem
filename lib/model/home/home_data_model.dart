6245d4cf4b3782da76bdae60d07996ac059e21b8
元 <123@163.com>
2021-05-23 23:47:04 +0800

    首页修改

---
+++ lib/model/home/home_data_model.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+// row cell 数据
+class HomeDataModel {
+  final int id; // 机台序号
+  final String customerModel; //客户型号
+  final String productSpecification; //产品规格
+  final String processing; //加工工序
+  final int dailyOutput; // 当天产量
+  final int numberChanges; // 换机次数
+  final String chief; //负责人
+  final String qualityInspector; //质检员
+  final String remarks; //备注
+
+  const HomeDataModel({
+    required this.id, // 机台序号
+    required this.customerModel, //客户型号
+    required this.productSpecification, //产品规格
+    required this.processing, //加工工序
+    required this.dailyOutput, // 当天产量
+    required this.numberChanges, // 换机次数
+    required this.chief, //负责人
+    required this.qualityInspector, //质检员
+    required this.remarks, //备注
+  });
+
+  factory HomeDataModel.fromJson(Map<String, dynamic> json) {
+    return HomeDataModel(
+      id: json['id'] as int,
+      customerModel: json['customerModel'] as String,
+      productSpecification: json['productSpecification'] as String,
+      processing: json['processing'] as String,
+      chief: json['chief'] as String,
+      qualityInspector: json['qualityInspector'] as String,
+      remarks: json['remarks'] as String,
+      dailyOutput: json['dailyOutput'] as int,
+      numberChanges: json['numberChanges'] as int,
+    );
+  }
+}
+
+// column标题
+class HomeTitleModel{
+  final String titleNumber; // 机台序号
+  final String titleCustomerModel; //客户型号
+  final String titleProductSpecification; //产品规格
+  final String titleProcessing; //加工工序
+  final String titleDailyOutput; // 当天产量
+  final String titleNumberChanges; // 换机次数
+  final String titleChief; //负责人
+  final String titleQualityInspector; //质检员
+  final String titleRemarks; //备注
+
+  const HomeTitleModel({
+    required this.titleNumber, // 机台序号
+    required this.titleCustomerModel, //客户型号
+    required this.titleProductSpecification, //产品规格
+    required this.titleProcessing, //加工工序
+    required this.titleDailyOutput, // 当天产量
+    required this.titleNumberChanges, // 换机次数
+    required this.titleChief, //负责人
+    required this.titleQualityInspector, //质检员
+    required this.titleRemarks, //备注
+  });
+
+  factory HomeTitleModel.fromTitleJson(Map<String, dynamic> json) {
+    return HomeTitleModel(
+      titleNumber: json['titleNumber'] as String,
+      titleCustomerModel: json['titleCustomerModel'] as String,
+      titleProductSpecification: json['titleProductSpecification'] as String,
+      titleProcessing: json['titleProcessing'] as String,
+      titleDailyOutput: json['titleDailyOutput'] as String,
+      titleNumberChanges: json['titleNumberChanges'] as String,
+      titleChief: json['titleChief'] as String,
+      titleQualityInspector: json['titleQualityInspector'] as String,
+      titleRemarks: json['titleRemarks'] as String,
+    );
+  }
+}
--- lib/widge/home/home_table_widget.dart
+++ lib/widge/home/home_table_widget.dart
@@ -1,42 +1,61 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:universe/model/home/home_data_model.dart';
 
 class GenerateTable extends StatefulWidget {
-  // const GenerateTable({Key key}) : super(key: key);
-
-  static const routeName = '/table';
-
   @override
   _GenerateTableState createState() => _GenerateTableState();
 }
 
 class _GenerateTableState extends State<GenerateTable> {
-  List<User> _userList = [];
+
+  List<HomeDataModel> _dataList = [];
+   HomeTitleModel? homeTitleModel;
+
+
   fetchUsers() async {
-    var data = await http
-        .get(Uri.parse('https://next.json-generator.com/api/json/get/4kWGoCxF9'));
 
-    var jsonData = json.decode(data.body);
+    var titleData = await http.get(
+        Uri.parse('https://next.json-generator.com/api/json/get/E1HniUQF9'));
+    if (titleData.statusCode == 200) {
 
-    List<User> users = [];
+      Map<String, dynamic>  titles = json.decode(titleData.body);
 
-    for (var i in jsonData) {
-      User user = User(i['name'], i['gender'], i['age'], i['email'],
-          i['eyeColor'], i['phone'], i['company']);
-      users.add(user);
-      print(user.eyeColor);
+
+      setState(() {
+        var _homeTitleModel = new HomeTitleModel.fromTitleJson(titles);
+        // homeTitleModel = _homeTitleModel;
+        // // Map<String, dynamic> myMap = new Map<String, dynamic>.from(titles);
+        // // homeTitleModel = titles.map((json) => HomeTitleModel.fromTitleJson(json));
+        // homeTitleModel = Map<String, dynamic>.from(titles); => HomeTitleModel.fromTitleJson(json))
+      });
+    } else {
+      print("err code $titleData.statusCode");
     }
-    print(users.length);
-    this.setState(() {
-      _userList = users;
-    });
+
+    var data = await http.get(
+        Uri.parse('https://next.json-generator.com/api/json/get/4J__XazFq'));
+    if (data.statusCode == 200) {
+      List top = json.decode(data.body);
+      setState(() {
+        _dataList = top.map((json) => HomeDataModel.fromJson(json)).toList();
+      });
+    } else {
+      print("err code $data.statusCode");
+    }
   }
 
   @override
   void initState() {
-    fetchUsers();
+    // fetchUsers().whenCom;
     super.initState();
+
+    fetchUsers().whenComplete((){
+      setState(() {});
+    });
   }
 
   @override
@@ -50,9 +69,7 @@ class _GenerateTableState extends State<GenerateTable> {
           //     fontSize: 50,
           //   ),
           // ),
-          Container(
-
-          ),
+          Container(),
 
           Expanded(
             child: Container(
@@ -75,53 +92,48 @@ class _GenerateTableState extends State<GenerateTable> {
                   scrollDirection: Axis.horizontal,
                   child: DataTable(
                     columns: [
-                      DataColumn(label: Text("机台序号")),
-                      DataColumn(label: Text("客户型号")),
-                      DataColumn(label: Text("产品规格")),
-                      DataColumn(label: Text("加工工序")),
-                      DataColumn(label: Text("当天产量")),
-                      DataColumn(label: Text("换机次数")),
-                      DataColumn(label: Text("负责人")),
-                      DataColumn(label: Text("质检员")),
-                      DataColumn(label: Text("备注")),
-                    ],
-                    rows: _userList
-                        .map(
-                      ((val) => DataRow(
-                        cells: <DataCell>[
-                          DataCell(Text(val.name.toString())),
-                          DataCell(Text(val.gender)),
-                          DataCell(Text(val.age.toString())),
-                          DataCell(Text(val.email)),
-                          DataCell(Text(val.eyeColor)),
-                          DataCell(Text(val.phone)),
-                          DataCell(Text(val.company)),
-                          DataCell(Text("")),
-                          DataCell(Text("")),
-                        ],
-                      )),
-                    )
+                      // DataColumn(label: Text(homeTitleModel!.titleNumber)),
+                      // DataColumn(label: Text(homeTitleModel!.titleCustomerModel)),
+                      // DataColumn(label: Text(homeTitleModel!.titleProductSpecification)),
+                      // DataColumn(label: Text(homeTitleModel!.titleProcessing)),
+                      // DataColumn(label: Text(homeTitleModel!.titleDailyOutput)),
+                      // DataColumn(label: Text(homeTitleModel!.titleNumberChanges)),
+                      // DataColumn(label: Text(homeTitleModel!.titleChief)),
+                      // DataColumn(label: Text(homeTitleModel!.titleQualityInspector)),
+                      // DataColumn(label: Text(homeTitleModel!.titleRemarks)),
+
+
+                      DataColumn(label: Text("")),
+                      DataColumn(label: Text("homeTitleModel")),
+                      DataColumn(label: Text("homeTitleModel.titleProductSpecification")),
+                      DataColumn(label: Text("homeTitleModel")),
+                      DataColumn(label: Text("homeTitleModel")),
+                      DataColumn(label: Text("homeTitleModel")),
+                      DataColumn(label: Text("homeTitleModel")),
+                      DataColumn(label: Text("homeTitleModel")),
+                      DataColumn(label: Text("homeTitleModel")),
+                      ],
+                    rows: _dataList
+                        .map((homeModel) => DataRow(cells: <DataCell>[
+                              DataCell(Text(homeModel.id.toString())),
+                              DataCell(Text(homeModel.customerModel)),
+                              DataCell(Text(homeModel.productSpecification)),
+                              DataCell(Text(homeModel.processing)),
+                              DataCell(Text(homeModel.dailyOutput.toString())),
+                              DataCell(
+                                  Text(homeModel.numberChanges.toString())),
+                              DataCell(Text(homeModel.chief)),
+                              DataCell(Text(homeModel.qualityInspector)),
+                              DataCell(Text(homeModel.remarks)),
+                            ]))
                         .toList(),
                   ),
                 ),
               ),
             ),
-          ),
+          )
         ],
       ),
     );
   }
-}
-
-class User {
-  final int name;
-  final String gender;
-  final int age;
-  final String email;
-  final String eyeColor;
-  final String phone;
-  final String company;
-
-  User(this.name, this.gender, this.age, this.email, this.eyeColor, this.phone,
-      this.company);
 }
